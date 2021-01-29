587508d7860b15e8990f7787780fe7c2917589ff
FARAHAISHAHSUHA\User <farahaishah811@gmail.com>
2021-01-29 23:44:57 +0800

    CREATE SPLASHSCREEN AND LOGIN

---
+++ app/src/main/java/com/example/group2/Splashscreen.java
@@ -0,0 +1,27 @@
+package com.example.group2;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashscreen extends AppCompatActivity {
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splashscreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+
+    }
+}
