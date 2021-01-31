c28b94a04ada900e406ad00f86454ec97a00bae1
MASYITAH MUSTAPHA <masyitah1505@gmail.com>
2021-01-31 18:13:05 +0800

    CREATE NEW CREDENTIALS AND PASSWORD UPDATED

---
+++ app/src/main/java/com/example/group2/NewCredentials.java
@@ -0,0 +1,13 @@
+package com.example.group2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewCredentials extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_credentials);
+    }
+}
