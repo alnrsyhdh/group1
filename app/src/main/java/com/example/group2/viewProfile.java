aefb9cfc38cb7eba7acd47ecbd60d6a3b077cd60
AISYAHNADIA\60194 <aisyahnadia23@gmail.com>
2021-02-01 16:40:31 +0800

    UPDATE VIEW PROFILE PHOTO

--- app/src/main/java/com/example/group2/viewProfile.java
+++ app/src/main/java/com/example/group2/viewProfile.java
@@ -3,10 +3,7 @@ package com.example.group2;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
+
 
 public class viewProfile extends AppCompatActivity {
 
