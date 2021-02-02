118c240e1843ac613c911cf5c552477cfa91a664
AISYAHNADIA\60194 <aisyahnadia23@gmail.com>
2021-02-02 16:32:04 +0800

    REUPLOAD NEW CODE UPDATE CHANGE PROFILE PICTURE

---
+++ app/src/main/java/com/example/group2/changeProfilePhoto.java
@@ -0,0 +1,14 @@
+package com.example.group2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class changeProfilePhoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_profile_photo);
+    }
+}
