4d9efc0407eba5c49664d8ac52668f1e0dd4ccb7
FARAHAISHAHSUHA\User <farahaishah811@gmail.com>
2021-02-28 23:33:54 +0800

    CREATE RENTAL FORM XML

--- app/src/main/java/com/example/group2/MenuProfile.java
+++ app/src/main/java/com/example/group2/MenuProfile.java
@@ -24,107 +24,107 @@ public class MenuProfile extends AppCompatActivity implements NavigationView.OnN
 
 
 
-        //variables
-        TextView dress,suit,shoes;
-        DrawerLayout drawerLayout;
-        NavigationView navigationView;
-        Toolbar toolbar;
+    //variables
+    TextView dress,suit,shoes;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_menu_profile);
-
-
-            //hooks
-            drawerLayout = findViewById(R.id.drawer_layout);
-            navigationView = findViewById(R.id.nav_view);
-            toolbar = findViewById(R.id.toolbar);
-            dress= findViewById(R.id.dress);
-            suit= findViewById(R.id.suit);
-            shoes= findViewById(R.id.shoes);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_profile);
 
 
-            //toolbar
-            setSupportActionBar(toolbar);
+        //hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        dress= findViewById(R.id.dress);
+        suit= findViewById(R.id.suit);
+        shoes= findViewById(R.id.shoes);
 
-            dress.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    startActivity(new Intent(MenuProfile.this, ViewDress.class));
-                }
-            });
 
-           suit.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    startActivity(new Intent(MenuProfile.this, ViewSuit.class));
-                }
-            });
+        //toolbar
+        setSupportActionBar(toolbar);
 
-            shoes.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    startActivity(new Intent(MenuProfile.this, ViewShoes.class));
-                }
-            });
-
-            //nav_drawer menu
-            navigationView.bringToFront();
-            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-            drawerLayout.addDrawerListener(toggle);
-            toggle.syncState();
-
-            navigationView.setNavigationItemSelectedListener(this);
-        }
-
-        @Override
-        public void onBackPressed() {
-
-            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                drawerLayout.closeDrawer(GravityCompat.START);
-            } else {
-                super.onBackPressed();
+        dress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MenuProfile.this, ViewDress.class));
             }
-        }
+        });
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-
-            switch (menuItem.getItemId()) {
-                case R.id.book: //bookNow
-                    startActivity(new Intent(MenuProfile.this, MenuProfile.class));
-                    break;
-
-                case R.id.settingsMenu:
-                    startActivity(new Intent(MenuProfile.this, ViewProfile.class));
-                    break;
-
-                case R.id.AboutUs:
-                    startActivity(new Intent(MenuProfile.this, AboutUs.class));
-                    break;
-
-                case R.id.clothes_menu:
-                    startActivity(new Intent(MenuProfile.this, MenuProfile.class));
-                    break;
-
-                case R.id.contactUs: //contactUs punya part
-                    startActivity(new Intent(MenuProfile.this, ContactUs.class));
-                    break;
-
-                case R.id.history: //Order History punya part
-                    startActivity(new Intent(MenuProfile.this, MenuProfile.class));
-                    break;
-
-                case R.id.feedback: //customer feedback punya part
-                    startActivity(new Intent(MenuProfile.this, CustFeedback.class));
-                    break;
-
-
+        suit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MenuProfile.this, ViewSuit.class));
             }
-            return true;
-        }
+        });
 
+        shoes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MenuProfile.this, ViewShoes.class));
+            }
+        });
 
+        //nav_drawer menu
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+            case R.id.book: //bookNow
+                startActivity(new Intent(MenuProfile.this, MenuProfile.class));
+                break;
+
+            case R.id.settingsMenu:
+                startActivity(new Intent(MenuProfile.this, ViewProfile.class));
+                break;
+
+            case R.id.AboutUs:
+                startActivity(new Intent(MenuProfile.this, AboutUs.class));
+                break;
+
+            case R.id.clothes_menu:
+                startActivity(new Intent(MenuProfile.this, MenuProfile.class));
+                break;
+
+            case R.id.contactUs: //contactUs punya part
+                startActivity(new Intent(MenuProfile.this, ContactUs.class));
+                break;
+
+            case R.id.history: //Order History punya part
+                startActivity(new Intent(MenuProfile.this, MenuProfile.class));
+                break;
+
+            case R.id.feedback: //customer feedback punya part
+                startActivity(new Intent(MenuProfile.this, CustFeedback.class));
+                break;
+
+
+        }
+        return true;
+    }
+
+
+
+}
